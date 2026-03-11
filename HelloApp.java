fd1379d4fc4a8527a01b6b466d5f932d9511d875
umangpandey1661 <umangpandey@Umangs-MacBook-Air-5.local>
2026-03-11 11:43:52 +0530

    Coded UC2: Display Hello with Command-Line Argument

--- HelloApp.java
+++ HelloApp.java
@@ -1,6 +1,7 @@
 public class HelloApp {
-    public static void main(String[] args){
-        System.out.println("Hello,World!");
-    }
-    
+public static void main (String[] args) {
+
+String name = args [0];
+System.out.println("Hello, " + name + "!");
+}
 }
